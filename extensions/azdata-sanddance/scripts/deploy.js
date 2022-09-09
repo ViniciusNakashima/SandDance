bfc49e4edbcf642b5813fb9e8f03fc5f6256022f
Dan Marshall <danmar@microsoft.com>
2022-09-08 17:49:17 -0700

    remove root level files

--- extensions/azdata-sanddance/scripts/deploy.js
+++
@@ -1,4 +0,0 @@
-const fs = require('fs');
-const package = require('../package.json');
-const filename = `${package.name}-${package.version}.vsix`;
-fs.copyFileSync(`./${filename}`, `../../docs/dist/azdata/v4/${filename}`);
